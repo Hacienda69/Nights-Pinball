b5c04e84eaaf0deef2c9f17c1bc16890806d6df4
Hacienda69 <fannumero1dehacienda@gmail.com>
2022-11-15 13:14:44 +0100

    inicial comosellame
    subo la base

---
+++ Application.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "p2List.h"
+#include "Globals.h"
+
+class Module;
+class ModuleRender;
+class ModuleWindow;
+class ModuleTextures;
+class ModuleInput;
+class ModuleAudio;
+class ModulePlayer;
+class ModulePhysics;
+class ModuleFadeToBlack;
+class ModuleSceneTitle;
+class ModuleSceneIntro;
+
+class Application
+{
+public:
+	ModuleRender* renderer;
+	ModuleWindow* window;
+	ModuleTextures* textures;
+	ModuleInput* input;
+	ModuleAudio* audio;
+	ModulePlayer* player;
+	ModuleSceneIntro* scene_intro;
+	ModulePhysics* physics;
+	ModuleFadeToBlack* fade_to_black;
+	ModuleSceneTitle* scene_title;
+
+private:
+
+	p2List<Module*> list_modules;
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+private:
+
+	void AddModule(Module* mod);
+};
---
+++ ModuleFadeToBlack.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "SDL\include\SDL_rect.h"
+
+class ModuleFadeToBlack : public Module
+{
+public:
+	//Constructor
+	ModuleFadeToBlack(Application* app, bool start_enabled = true);
+	~ModuleFadeToBlack();
+
+	bool Start();
+	update_status Update();
+	update_status PostUpdate();
+
+	// Called from another module
+	// Starts the fade process which has two steps, fade_out and fade_in
+	// After the first step, the modules should be switched
+	bool FadeToBlack(Module* toDisable, Module* toEnable, float frames = 60);
+
+private:
+
+	enum Fade_Step
+	{
+		NONE,
+		TO_BLACK,
+		FROM_BLACK
+	} currentStep = Fade_Step::NONE;
+
+	// A frame count system to handle the fade time and ratio
+	Uint32 frameCount = 0;
+	Uint32 maxFadeFrames = 0;
+
+	// The rectangle of the screen, used to render the black rectangle
+	SDL_Rect screenRect;
+
+	// The modules that should be switched after the first step
+	Module* moduleToEnable = nullptr;
+	Module* moduleToDisable = nullptr;
+};
